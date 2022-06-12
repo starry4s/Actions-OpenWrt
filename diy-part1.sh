ac99a126dc8f4d57fde5bf4f6140dab88b6d61cb
starry4s <83576692+starry4s@users.noreply.github.com>
2022-06-12 22:17:59 +0800

    Create diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # openclash
 sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
